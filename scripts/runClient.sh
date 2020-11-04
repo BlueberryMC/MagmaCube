cd4c9f34de9b788a5daf49a2fc288d1426157777
acrylic-style <me@acrylicstyle.xyz>
2020-11-04 23:48:30 +0900

    make it easier to run runClient.sh

--- scripts/runClient.sh
+++ scripts/runClient.sh
@@ -6,4 +6,8 @@ mcpath="$basedir/Minecraft/target/Minecraft.jar"
 cd "$basedir/Minecraft" || exit 1
 version=$(git rev-parse HEAD | head -c 10)
 cd "$basedir/work/testClient" || exit 1
-javaw -Dlog4j.configuration=log4j2.xml -Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -cp "$mcpath" net.minecraft.client.main.Main --version="$version" "$@"
+ASSETS=~/.minecraft/assets
+if [[ "$OSTYPE" == *"win"* || "$OSTYPE" == "msys" ]]; then
+  ASSETS=~/AppData/Roaming/.minecraft/assets
+fi
+javaw -Dlog4j.configuration=log4j2.xml -Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -cp "$mcpath" net.minecraft.client.main.Main --version="$version" --assetIndex=1.16 --versionType debug --username="Dev" --uuid="abc" --accessToken="abc" --assetsDir="$ASSETS" --gameDir="$basedir/work/testClient" "$@"
