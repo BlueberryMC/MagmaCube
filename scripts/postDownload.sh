e2d16046975a4da83dd3eb2b412d321ce7cd39c3
acrylic-style <me@acrylicstyle.xyz>
2021-06-11 14:41:09 +0900

    Attempt to fix linux builds

--- scripts/postDownload.sh
+++ scripts/postDownload.sh
@@ -4,6 +4,6 @@ basedir="$(cd "$1" && pwd -P)"
 decompOutput="$basedir"/work/Minecraft/$version/source
 git="git -c commit.gpgsign=false"
 echo "Processing files at $decompOutput"
-# it actually removes some of unnecessary/wrong casts
-java -Xmx256M -jar "$basedir/work/UnusedCommentRemover/UnusedCommentRemover-1.0-SNAPSHOT.jar" "$decompOutput"
+# it removes some unnecessary/wrong casts and "fix"es invalid characters
+java -Xmx512M -jar "$basedir/work/UnusedCommentRemover/UnusedCommentRemover-1.0.2.jar" "$decompOutput"
 ./scripts/copyFiles.sh
