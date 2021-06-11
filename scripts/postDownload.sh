5a9a3dec9dc022aca67e7cf1c67bd135746a4cdf
acrylic-style <me@acrylicstyle.xyz>
2021-06-11 15:31:54 +0900

    process that characters in postDownload.sh

--- scripts/postDownload.sh
+++ scripts/postDownload.sh
@@ -5,5 +5,5 @@ decompOutput="$basedir"/work/Minecraft/$version/source
 git="git -c commit.gpgsign=false"
 echo "Processing files at $decompOutput"
 # it removes some unnecessary/wrong casts and "fix"es invalid characters
-java -Xmx512M -jar "$basedir/work/UnusedCommentRemover/UnusedCommentRemover-1.0.4.jar" "$decompOutput"
+java -Xmx512M -jar "$basedir/work/UnusedCommentRemover/UnusedCommentRemover-1.0.5.jar" "$decompOutput"
 ./scripts/copyFiles.sh
