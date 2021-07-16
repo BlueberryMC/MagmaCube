453eeb8d0a115271702d689d322b4c1a228b4b58
acrylic-style <me@acrylicstyle.xyz>
2021-07-16 20:39:54 +0900

    Update to 1.18 Experimental Snapshot 1

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-version=1.17.1
+version=1.18_experimental-snapshot-1
+name="1.18 Experimental Snapshot 1"
+releaseTarget="1.18"
+stable="false"
+seriesId="ccpreview"
 basedir="$(pwd -P)/"
-WORLD_VERSION=2730
-PROTOCOL_VERSION=756
+WORLD_VERSION=2825
+PROTOCOL_VERSION=1073741865
 RESOURCE_VERSION=7
 DATA_VERSION=7
