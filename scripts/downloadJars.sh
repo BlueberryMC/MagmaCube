194b540358d372d772e26e426af260911f43fac7
acrylic-style <me@acrylicstyle.xyz>
2021-11-18 12:26:42 +0900

    Update to 1.18 Pre-release 3

--- scripts/downloadJars.sh
+++ scripts/downloadJars.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source ./scripts/functions.sh
-clientJarUrl="https://launcher.mojang.com/v1/objects/020aa79e63a7aab5d6f30e5ec7a6c08baee6b64c/client.jar"
+clientJarUrl="https://launcher.mojang.com/v1/objects/fe58c0cffa004dbb78c9a1cf30cc935d280a45b3/client.jar"
 clientJarPath="$basedir"/work/Minecraft/$version/client.jar
-clientMappingUrl="https://launcher.mojang.com/v1/objects/8761adc5fc85673c238fd1b5a278cb143a47c678/client.txt"
+clientMappingUrl="https://launcher.mojang.com/v1/objects/8fa21e752f7cf46beed5c5989793cb6c25bc5349/client.txt"
 clientMappingPath="$basedir"/work/Minecraft/$version/mapping.txt
 clientRemappedJarPath="$basedir"/work/Minecraft/$version/client-remapped.jar
 FF_URL="https://maven.minecraftforge.net/net/minecraftforge/forgeflower/1.5.498.14/forgeflower-1.5.498.14.jar"
@@ -38,9 +38,9 @@ curl $clientMappingUrl --output "$clientMappingPath" || (
 )
 echo "Applying mapping"
 "$basedir"/work/MC-Remapper/bin/MC-Remapper --fixlocalvar=rename --output-name="$clientRemappedJarPath" "$clientJarPath" "$clientMappingPath" || exit 1
-java -Xmx2G -jar "$basedir/work/ParameterRemapper/ParameterRemapper-1.0.2.jar" --input-file="$clientRemappedJarPath" --output-file="$clientRemappedJarPath.2" --mapping-file="$basedir/work/mappings/mappings/$MAPPINGS_VERSION.pr" || exit 1
+java -Xmx2G -jar "$basedir/work/ParameterRemapper/ParameterRemapper-1.0.3.jar" --input-file="$clientRemappedJarPath" --output-file="$clientRemappedJarPath.2" --mapping-file="$basedir/work/mappings/mappings/$MAPPINGS_VERSION.pr" || exit 1
 echo "Applying AccessTransformers"
-java -Xmx2G -jar "$basedir/work/AccessTransformers/accesstransformers-3.0.1-fatjar.jar" --inJar "$clientRemappedJarPath.2" --outJar "$clientRemappedJarPath" --atFile "$basedir/work/mappings/mappings/$MAPPINGS_VERSION.at" || exit 1
+java -Xmx2G -jar "$basedir/work/AccessTransformers/accesstransformers-8.0.4-fatjar.jar" --inJar "$clientRemappedJarPath.2" --outJar "$clientRemappedJarPath" --atFile "$basedir/work/mappings/mappings/$MAPPINGS_VERSION.at" || exit 1
 echo "Deleting intermediate jar"
 rm "$clientRemappedJarPath.2"
 echo "Unpacking jar..."
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-version=1.18-pre1
-name="1.18 Pre-release 1"
+version=1.18-pre3
+name="1.18 Pre-release 3"
 releaseTarget="1.18"
 stable="false"
 seriesId="main"
 basedir="$(pwd -P)/"
-WORLD_VERSION=2847
-PROTOCOL_VERSION=1073741873
+WORLD_VERSION=2849
+PROTOCOL_VERSION=1073741875
 RESOURCE_VERSION=8
 DATA_VERSION=8
 MAPPINGS_VERSION=21w41a
