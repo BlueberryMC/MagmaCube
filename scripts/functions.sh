037378b4f7ec11f3b4cc3e211695f43be7a7ea70
acrylic-style <me@acrylicstyle.xyz>
2022-03-04 14:41:39 +0900

    Update to 1.18.2

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-version=1.18.2-rc1
-name="1.18.2 Release Candidate 1"
+version=1.18.2
+name="1.18.2"
 releaseTarget="1.18.2"
-stable="false"
+stable="true"
 seriesId="main"
 basedir="$(pwd -P)/"
-WORLD_VERSION=2874
-PROTOCOL_VERSION=1073741897
+WORLD_VERSION=2875
+PROTOCOL_VERSION=758
 RESOURCE_VERSION=8
 DATA_VERSION=9
 MAPPINGS_VERSION=22w05a
