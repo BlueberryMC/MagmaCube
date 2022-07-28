7ea5bc1793731506ab50b140d6c256c0f2621366
acrylic-style <me@acrylicstyle.xyz>
2022-07-28 13:01:40 +0900

    Update to 1.19.1

--- scripts/downloadJars.sh
+++ scripts/downloadJars.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source ./scripts/functions.sh
-clientJarUrl="https://piston-data.mojang.com/v1/objects/f8291ce57a856c0e4ca6f02af3eded3148d3e70c/client.jar"
+clientJarUrl="https://piston-data.mojang.com/v1/objects/90d438c3e432add8848a9f9f368ce5a52f6bc4a7/client.jar"
 clientJarPath="$basedir"/work/Minecraft/$version/client.jar
-clientMappingUrl="https://piston-data.mojang.com/v1/objects/2294071d96e59cf77fe8193576e97a8133e2c47b/client.txt"
+clientMappingUrl="https://piston-data.mojang.com/v1/objects/fc8e22d42c0e4eb1899e2acf7e97eae917e1cb94/client.txt"
 clientMappingPath="$basedir"/work/Minecraft/$version/mapping.txt
 clientRemappedJarPath="$basedir"/work/Minecraft/$version/client-remapped.jar
 FF_URL="https://maven.minecraftforge.net/net/minecraftforge/forgeflower/1.5.498.29/forgeflower-1.5.498.29.jar"
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-version=1.19.1-rc2
-name="1.19.1 Release Candidate 2"
+version=1.19.1
+name="1.19.1"
 releaseTarget="1.19.1"
-stable="false"
+stable="true"
 seriesId="main"
 basedir="$(pwd -P)/"
-WORLD_VERSION=3115
-PROTOCOL_VERSION=1073741924
+WORLD_VERSION=3117
+PROTOCOL_VERSION=760
 RESOURCE_VERSION=9
 DATA_VERSION=10
 MAPPINGS_VERSION=22w05a
