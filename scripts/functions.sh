57d8c54fed06e54b8bde38d569b71fb78bca2cc4
acrylic-style <me@acrylicstyle.xyz>
2023-06-17 10:02:22 +0900

    Update to 1.20.1

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-version=1.19.4
-name="1.19.4"
-releaseTarget="1.19.4"
+version=1.20.1
+name="1.20.1"
+releaseTarget="1.20.1"
 stable="true"
 seriesId="main"
 basedir="$(pwd -P)/"
-WORLD_VERSION=3337
-PROTOCOL_VERSION=762
-RESOURCE_VERSION=13
-DATA_VERSION=12
+WORLD_VERSION=3465
+PROTOCOL_VERSION=763
+RESOURCE_VERSION=15
+DATA_VERSION=15
 MAPPINGS_VERSION=22w05a
